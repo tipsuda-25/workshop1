c0cbb3c8a678b741ef0c0f3c7bc7da33cd2af66e
tipsuda-25 <tipsuda.ri@ku.th>
2021-01-10 11:34:00 +0700

    no message

---
+++ comment/comment/variables/global_ex_3.py
@@ -0,0 +1,6 @@
+def myfunc():
+    global x
+    x = "fantastic"
+
+myfunc()
+print("Python is " + x)
---
+++ comment/comment/variables/global_ex_4.py
@@ -0,0 +1,10 @@
+x = "awesome"
+
+def myfunc():
+    global x
+    print("Python is " + x)
+    x = "fantastic"
+    
+
+myfunc()
+print("Python is " + x)
