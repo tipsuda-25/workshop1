406bf4112e340ec9d9978e999912a2089e31a3d0
tipsuda-25 <tipsuda.ri@ku.th>
2021-01-10 11:11:34 +0700

    no message

---
+++ comment/comment/variables/mutiple_word_name.py
@@ -0,0 +1,8 @@
+# camel case
+myVariableName = "tipsuda Rienthong"
+
+# pascal case
+MyVariableName = "tipsuda rienthong"
+
+#Snake case
+my_variable_name = "tipsuda rienthong"
---
+++ comment/comment/variables/tip.py
@@ -0,0 +1,12 @@
+#สามารถประกาสได้
+myvar = "tip"
+my_var = "tip"
+_my_var_ "tip" 
+myVar = "tip"
+MYVAR = "tip"
+myvar2 = "tip"
+
+#ไม่สามารถประกาศได้
+2myvar = "tip"
+my-var = "tip"
+my var = "tip"
